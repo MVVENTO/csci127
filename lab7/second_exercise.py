7ab27938ac8930e1311638f8bc062312c0109171
zorozy@umich.edu <jim.morris.shen@gmail.com>
2017-10-19 17:40:16 -0400

    add the content of lab7wq

---
+++ lab7/second_exercise.py
@@ -0,0 +1,14 @@
+#me:  your name here
+#Date:  October 2017
+#This program, uses functions, says hello to the world!
+
+def main(number_of_prints):
+    for i in range(number_of_prints):
+        print("Hello, World!") 
+    
+if __name__ == "__main__":
+    number_of_prints=3
+    main(number_of_prints)
+    number_of_prints=1
+    main(number_of_prints)
+    

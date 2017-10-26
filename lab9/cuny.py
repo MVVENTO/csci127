0b3822ff3b5e86aa2a8789898815be2d4657a77a
Jimmy Shen <jim.morris.shen@gmail.com>
2017-10-26 17:28:51 -0400

    add lab 9

---
+++ lab9/cuny.py
@@ -0,0 +1,19 @@
+import folium
+import pandas as pd
+
+
+cuny = pd.read_csv('cunyLocations.csv')
+print (cuny["Campus"])
+
+mapCUNY = folium.Map(location=[40.768731, -73.964915])
+
+for index,row in cuny.iterrows():
+    lat = row["Latitude"]
+    lon = row["Longitude"]
+    name = row["Campus"]
+    newMarker = folium.Marker([lat, lon], popup=name)
+    newMarker.add_to(mapCUNY)
+#For each row in the file, we find the latitude, longitude, and name of the campus, and use those to create a new marker which we add to our map. We repeat for each row, until we have made markers for all 23 campuses in the file.
+
+
+mapCUNY.save(outfile='cunyLocations.html')

758236e375acc4c83bc0a6d7f494b9d18fd5128e
jimmy shen <jim.morris.shen@gmail.com>
2018-03-08 16:54:41 -0500

    update lab6

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -2,5 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 pop = pd.read_csv('nycHistPop.csv',skiprows=5)
 print(pop)
-pop.plot(x="Year")
+pop["frac"] = pop['Manhattan']/pop['Total']
+print(pop)
+pop.plot(x="Year", y ="frac")
 plt.show()

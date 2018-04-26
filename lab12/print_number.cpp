9a326fddadae8bdce1ae268249f5ed5317365f20
jimmy shen <jim.morris.shen@gmail.com>
2018-04-26 16:07:14 -0400

    update

---
+++ lab12/print_number.cpp
@@ -0,0 +1,16 @@
+//Name:  Your name here
+//Date:  November 2017
+//My first program in C++
+
+#include <iostream>            //The built-in library for input/output
+using namespace std; //The names of standard definitions
+
+int main ()          //C++ programs all have a main() function
+{
+unsigned int8 a=4294967295;
+cout << a<<endl;  
+a+=1;
+
+cout << a<<endl;  
+return 0;          //Standard way to end function 
+}
